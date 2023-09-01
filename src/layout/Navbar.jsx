3784352b8f280d8dc09cbf9bcb0650b918cdcf57
Basil <ohuruogubasil@gmail.com>
2023-09-01 11:57:03 +0100

    added:setup

---
+++ src/layout/Navbar.jsx
@@ -0,0 +1,19 @@
+import { Link } from "react-router-dom";
+
+const Navbar = () => {
+  return (
+    <div>
+      <nav className="navbar navbar-expand-lg navbar-dark bg-primary">
+        <div className="container-fluid">
+          <Link to="/" className="navbar-brand"> Drug App </Link>
+          <Link to="/adddrug" className="btn btn-outline-light">
+            {" "}
+            Add Drug{" "}
+          </Link>
+        </div>
+      </nav>
+    </div>
+  );
+};
+
+export default Navbar;
---
+++ src/pages/Home.jsx
@@ -0,0 +1,16 @@
+import React, { useState } from 'react'
+import axios from 'axios'
+import { useNavigate } from 'react-router-dom'
+
+const Home = () => {
+    const [drugs, setDrugs] = useState([])
+    const navigate = useNavigate()
+
+  return (
+      <div>
+        
+    </div>
+  )
+}
+
+export default Home
